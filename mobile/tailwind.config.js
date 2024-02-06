5460a6802d07071733dc0428e4a54a7b2580c988
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-02-05 23:57:32 -0300

    ⚙️ config(mobile): setting fonts in tailwind and types

--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{ts, tsx}",
-    "./src/components/**/*.{ts, tsx}",
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: "Inter_400Regular",
+        bold: "Inter_700Bold",
+        heading: "Inter_600SemiBold",
+        subtitle: "Inter_500Medium",
+      }
+    },
   },
   plugins: [],
 }
-
