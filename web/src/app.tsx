aa0310ea319571a4528f81adbef68ed541dad3a8
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-02-09 21:17:41 -0300

    🎨 styles(frontend): create home screen
    - Create NewNoteCard and NoteCard component

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,7 +1,27 @@
+import logo from './assets/logo-nlw-expert.svg'
+
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="mx-auto max-w-6xl space-y-6">
+      <img alt="NLW Expert" src={logo} />
+
+      <form className="w-full">
+        <input
+          className="w-full bg-transparent font-semibold text-3xl tracking-tight placeholder:text-slate-500 outline-none"
+          placeholder="Busque em suas notas..."
+          type="text"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+        <NoteCard />
+      </div>
+    </div>
   )
 }
