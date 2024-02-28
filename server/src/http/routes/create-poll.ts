3b2f516842756c9b6e5a6f65b77f912ee9f8ca06
Guilherme Bezerra <gbsantos.it@gmail.com>
2024-02-28 17:20:05 -0300

    📐 refactor(backend): organize/separate files responsability

---
+++ server/src/http/routes/create-poll.ts
@@ -0,0 +1,25 @@
+import { FastifyInstance } from "fastify"
+import { prisma } from "../../lib/prisma"
+import { z } from 'zod'
+
+export async function createPoll(app: FastifyInstance) {
+  app.post('/polls', async (request, reply) => {
+    const createPollBody = z.object({
+      title: z.string()
+    })
+
+    const { title } = createPollBody.parse(request.body)
+
+    const poll = await prisma.poll.create({
+      data: {
+        title
+      }
+    })
+
+    return reply.status(201).send({ poll_id: poll.id })
+  })
+
+  app.get('/hello', () => {
+    return 'Hello NLW'
+  })
+}
